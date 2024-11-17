fbe56607959e9399c274a88081073a92d7f83f6d
Asadimra <asadimran031@gmail.com>
2024-11-17 17:18:03 +0500

    Resolve error on vercel

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
           } lg:flex flex-col lg:flex-row justify-end items-center font-serif text-xl absolute lg:static bg-indigo-950 lg:bg-transparent w-full lg:w-auto top-20 left-0 lg:top-0`}
         >
           <li className="list-none px-4 py-2 lg:py-0 text-white hover:text-purple-300">
-           <Link href="/Home">Home</Link>
+           <Link href="/HomePage">Home</Link>
           </li>
           <li className="list-none px-4 py-2 lg:py-0 text-white hover:text-purple-300">
             <Link href="/Skills">About</Link>
